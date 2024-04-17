3e3d71c958b1f621595edd11ac21b51a187190e8
ElCardoso <gr78787@gmail.com>
2024-04-16 21:00:37 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Olá, seja bem vindo!</Text>
+      <TextInput placeholder="Nome completo" style={styles.inputs}></TextInput>
+      <TextInput
+        placeholder="Seu melhor e-mail"
+        style={styles.inputs}
+      ></TextInput>
+      <TextInput placeholder="Telefone" style={styles.inputs}></TextInput>
+
       <StatusBar style="auto" />
+
+      <View>
+        <Button backgroundColor="red" title="Já tenho uma conta"></Button>
+        <Button style={styles.button} title="Quero me cadastrar"></Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 35,
+    marginBottom: 60,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 40,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  inputs: {
+    width: 380,
+    height: 60,
+    padding: 10,
+    borderRadius: 7,
+    borderColor: "gray",
+    borderWidth: 1,
   },
 });
