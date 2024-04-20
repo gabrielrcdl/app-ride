dea4bc41ffa6363a622b9a16eced3d5ecc48c610
douglasew <douglas_ewerton10@hotmail.com>
2024-04-19 22:05:20 -0300

    changing loginscreen

--- src/screens/LoginScreen/index.jsx
+++ src/screens/LoginScreen/index.jsx
@@ -1,54 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import ButtonComponent from "../../components/ButtonComponent";
 import InputComponent from "../../components/InputComponent";
+import { styles } from "./styles";
 
 const LoginScreen = () => {
-
   return (
     <View style={styles.container}>
-     
-
-
       <Text style={styles.title}>Carona fácil</Text>
       <View style={styles.inputsContainer}>
-
-      <InputComponent placeholder="E-mail" />
-      <InputComponent placeholder="Senha" />
-      <ButtonComponent title="Login" screen="Login" />
+        <InputComponent placeholder="E-mail" />
+        <InputComponent placeholder="Senha" />
+        <ButtonComponent title="Login" screen="Home" />
         <ButtonComponent title="Quero me cadastrar" screen="Register" />
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 35,
-    color: '#fff',
-    fontWeight: 'bold',
-    marginTop: 300,
-    fontSize: 30,
-    textTransform: 'uppercase'
-  },
-  container: {
-    flex: 2,
-    gap: 200,
-    backgroundColor: "black",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  inputsContainer: {
-    backgroundColor: "#fff",
-    gap: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 600,
-    borderTopRightRadius: '50px',
-    borderTopLeftRadius: '50px',
-    width: '100%',
-    
-  }
-});
 
 export default LoginScreen;
--- src/screens/LoginScreen/styles.js
+++ src/screens/LoginScreen/styles.js
@@ -3,13 +3,27 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   title: {
     fontSize: 35,
-    marginBottom: 60,
+    color: "#fff",
+    fontWeight: "bold",
+    marginTop: 300,
+    fontSize: 30,
+    textTransform: "uppercase",
   },
   container: {
-    flex: 1,
-    gap: 40,
-    backgroundColor: "#fff",
+    flex: 2,
+    gap: 200,
+    backgroundColor: "black",
     alignItems: "center",
     justifyContent: "center",
+  },
+  inputsContainer: {
+    backgroundColor: "#fff",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 600,
+    borderTopRightRadius: 50,
+    borderTopLeftRadius: 50,
+    width: "100%",
   },
 });
